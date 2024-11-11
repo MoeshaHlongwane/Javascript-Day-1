37ae6848ec6053ba374101140842f312007bd4b0
MoeshaHlongwane <nikiwehlongwane22@gmail.com>
2024-11-11 12:36:56 +0200

    git add .

---
+++ control-structures.js
@@ -0,0 +1,13 @@
+// there are 3 ways to implement conditional logic:
+// if else and else if statements 
+let myString = "Something".toLowerCase()
+
+if (myString == "something"){
+    console.log("The string is equal to Something");
+
+}else if(myString){
+
+}else {
+    console.log("The string is not equal to Something");        
+}
+
